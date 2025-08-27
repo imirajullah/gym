94f2031dc111df9f3d73b087c37fef99293f6841
Mirajullah <mirajullah95@gmail.com>
2025-08-27 19:33:18 +0500

    gym

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+import  './index.js';
+import './index.css';
+function App(){
+    return(
+    <div>
+        <h1>Hello World</h1>
+    </div>
+    );
+}
+export default App;
---
+++ src/mian.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Mian() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+export default Mian;
