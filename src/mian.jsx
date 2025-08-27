cc424acdfe298586d038ace5fc2fef8de029d884
Mirajullah <mirajullah95@gmail.com>
2025-08-27 19:48:30 +0500

    mian.jsx

--- src/mian.jsx
+++ src/mian.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./component/Home";
 
 function Mian() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+   return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+      </Routes>
+    </Router>
   );
 }
 export default Mian;
